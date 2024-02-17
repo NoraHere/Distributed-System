ababa7b5cd4b9eee9afec78e99a5b4f7dcaa0414
JY <e1237248@u.nus.edu>
2024-02-17 17:22:09 +0800

    cannot fresh alive server in two consecutive time

--- cs5223-labs/labs/lab2-primarybackup/src/dslabs/primarybackup/ViewServer.java
+++ cs5223-labs/labs/lab2-primarybackup/src/dslabs/primarybackup/ViewServer.java
@@ -4,6 +4,7 @@ import static dslabs.primarybackup.PingCheckTimer.PING_CHECK_MILLIS;
 
 import dslabs.framework.Address;
 import dslabs.framework.Node;
+import java.util.ArrayList;
 import java.util.Objects;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
@@ -11,21 +12,30 @@ import lombok.ToString;
 //self adding
 import java.util.HashMap;
 import java.util.Random;
+import java.util.LinkedHashSet;
+import java.util.HashSet;
+import java.util.Iterator;
 import dslabs.primarybackup.View;
+import org.checkerframework.checker.units.qual.A;
 
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 class ViewServer extends Node {
+
   static final int STARTUP_VIEWNUM = 0;
   private static final int INITIAL_VIEWNUM = 1;
 
   // Your code here...
   private View currentview;
-  private int currentviewNum;
+  private int currentViewNum;
   private Address cprimaryAdd;//current primary address
   private Address cbackupAdd;//current backup address
-  private HashMap<Address, Boolean> serveralive=new HashMap<>();//record alive server
-  private int serViewNum;//server most recent viewNum
+  private HashMap<Address, Integer> serveralive_f = new HashMap<>();//record alive server and view number it knows
+  private HashMap<Address, Integer> serveralive_s= new HashMap<>();//second consecutive time alive server
+  private int serViewNum;// most recent viewNum server knows
+
+  private int priViewNum;//primary acknowledge view number
+
   /* -----------------------------------------------------------------------------------------------
    *  Construction and Initialization
    * ---------------------------------------------------------------------------------------------*/
@@ -37,9 +47,12 @@ class ViewServer extends Node {
   public void init() {
     set(new PingCheckTimer(), PING_CHECK_MILLIS);
     // Your code here...
-    currentviewNum=STARTUP_VIEWNUM;
-    View iniview=new View(currentviewNum,null,null);//initial any primary server and a backup
-    currentview=iniview;
+    //moving to the above
+    currentViewNum = STARTUP_VIEWNUM;
+    cprimaryAdd=null;
+    cbackupAdd=null;
+    currentview = new View(currentViewNum, cprimaryAdd, cbackupAdd);
+
   }
 
   /* -----------------------------------------------------------------------------------------------
@@ -47,14 +60,55 @@ class ViewServer extends Node {
    * ---------------------------------------------------------------------------------------------*/
   private void handlePing(Ping m, Address sender) {
     // Your code here...
-    serViewNum =m.getViewNum();
-    serveralive.put(sender,true);//record this sender is alive
-    this.send(new ViewReply(currentview),sender);
+    serViewNum = m.getViewNum();
+    serveralive_f.put(sender, serViewNum);//record this sender is alive, and viewNum it knows
+
+
+    //initialize after first ping
+    if (Objects.equals(cprimaryAdd, null) && Objects.equals(currentViewNum, STARTUP_VIEWNUM)) {
+      currentViewNum = INITIAL_VIEWNUM;
+      cprimaryAdd = sender;
+      currentview = new View(currentViewNum, cprimaryAdd, cbackupAdd);
+    }else { //later stage
+
+      if(Objects.equals(sender,cprimaryAdd)){
+        priViewNum=serViewNum;
+      }
+      //primary acknowledges correct current view number
+      if (Objects.equals(priViewNum, currentViewNum)) {
+
+        //view service proceed to a new view
+        if (!Objects.equals(cbackupAdd, null)){//backup exist
+          if (!serveralive_f.containsKey(cprimaryAdd) && !serveralive_s.containsKey(cprimaryAdd)) {
+            //don't receive ping from primary
+            currentViewNum += 1;
+            cprimaryAdd = cbackupAdd;//make backup be new primary
+            cbackupAdd = selectServer(serveralive_f, serveralive_s, cprimaryAdd);
+            currentview = new View(currentViewNum, cprimaryAdd, cbackupAdd);
+          }
+          if (!serveralive_f.containsKey(cbackupAdd) && !serveralive_s.containsKey(cbackupAdd)) {
+            //don't receive ping from backup
+            currentViewNum += 1;
+            cbackupAdd = selectServer(serveralive_f, serveralive_s, cprimaryAdd);//select new backup
+            currentview = new View(currentViewNum, cprimaryAdd, cbackupAdd);
+          }
+        } else {//no backup
+          cbackupAdd = selectServer(serveralive_f, serveralive_s, cprimaryAdd);//select other than cprimary add
+          if (!Objects.equals(cbackupAdd, null)) {//has idle server
+            currentViewNum += 1;
+            currentview = new View(currentViewNum, cprimaryAdd, cbackupAdd);
+          }
+        }
+
+      }
+    }
+    this.send(new ViewReply(currentview), sender);
   }
 
   private void handleGetView(GetView m, Address sender) {
     // Your code here...
-    this.send(new ViewReply(currentview),sender);
+    //System.out.println(serveralive);
+    this.send(new ViewReply(currentview), sender);
   }
 
   /* -----------------------------------------------------------------------------------------------
@@ -62,49 +116,35 @@ class ViewServer extends Node {
    * ---------------------------------------------------------------------------------------------*/
   private void onPingCheckTimer(PingCheckTimer t) {
     // Your code here...
-    //decide whether server is alive/dead
-    if (Objects.equals(currentviewNum,0)){//Go to first state
-      cprimaryAdd=selectServer(serveralive);
-      serveralive.remove(cprimaryAdd);
-      cbackupAdd=selectServer(serveralive);
-      currentviewNum=INITIAL_VIEWNUM;
-      currentview=new View(currentviewNum,cprimaryAdd,cbackupAdd);
-      serveralive=new HashMap<>();//resetting status of whether receive a ping
-    }
-    if (!Objects.equals(cbackupAdd,null)) {
-      if (!serveralive.containsKey(cprimaryAdd)) {//if don't receive ping from primary
-        currentviewNum += 1;
-        cprimaryAdd = cbackupAdd;//make backup be new primary
-        serveralive.remove(cprimaryAdd);
-        cbackupAdd = selectServer(serveralive);
-        currentview = new View(currentviewNum, cprimaryAdd, cbackupAdd);
-      }
 
-      if (!serveralive.containsKey(cbackupAdd)) {//if don't receive ping from backup
-        currentviewNum += 1;
-        serveralive.remove(cprimaryAdd);
-        cbackupAdd = selectServer(serveralive);//select new backup
-        currentview = new View(currentviewNum, cprimaryAdd, cbackupAdd);
-      }
-    }else{//has idle server
-      //serveralive.containsKey()
-    }
+  //garbage collect server status over two consecutive time
+    serveralive_s.clear();
+    serveralive_s.putAll(serveralive_f);
+    serveralive_f.clear();
 
-    //send(Ping,)
+  //check within two consecutive PingCheckTimer
     this.set(t, PING_CHECK_MILLIS);
+
   }
 
   /* -----------------------------------------------------------------------------------------------
    *  Utils
    * ---------------------------------------------------------------------------------------------*/
   // Your code here...
-  private Address selectServer(HashMap<Address,Boolean> m){//select random server
-    Address[] keys = m.keySet().toArray(new Address[0]);
+  private Address selectServer(HashMap<Address,Integer> m1,HashMap<Address,Integer> m2,Address cprimaryAdd){//select random server
+    //Address[] keys = m.keySet().toArray(new Address[0]);
+    LinkedHashSet<Address> keys = new LinkedHashSet<>(m1.keySet());
+    keys.addAll(m2.keySet());//combine two consecutive time alive server without duplicate
+    ArrayList<Address> newkey=new ArrayList<>(keys);
+    newkey.remove(cprimaryAdd);
     // Generate a random index within the range of the number of keys
+    if (newkey.isEmpty()) return null;//no server to select
+    else if (newkey.size()==1) {//only one server available
+      return newkey.get(0);
+    }
     Random random = new Random();
-    int randomIndex = random.nextInt(keys.length);
-    Address randomKey = keys[randomIndex];//this is random selected primary
-    return randomKey;
+    int randomIndex = random.nextInt(newkey.size());
+    return newkey.get(randomIndex);//this is random selected primary
   }
 
 
